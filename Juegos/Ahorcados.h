681df8ea7660aa9873f5420c4f5feaef7d6cfe87
Daniel-codeCreator <dboc987@gmail.com>
2026-04-03 17:50:48 -0600

    Version2.0_20260403

---
+++ Juegos/Ahorcados.h
@@ -0,0 +1,14 @@
+//
+// Created by dboc9 on 2/04/2026.
+//
+
+#ifndef PROYFINALPROGRA1_AHORCADOS_H
+#define PROYFINALPROGRA1_AHORCADOS_H
+
+
+class Ahorcados
+{
+};
+
+
+#endif //PROYFINALPROGRA1_AHORCADOS_H
